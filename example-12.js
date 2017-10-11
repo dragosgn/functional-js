cc78f0146731fc0926c9c740c40c85df1f1fdf1f
Dragos <dragosgn@gmail.com>
2017-10-11 22:49:08 +0200

    forking

--- example-12.js
+++ example-12.js
@@ -10,9 +10,27 @@ Task.rejected(1) // acts just like a Box
 .fork(e => console.log("err", e),
       x => console.log("success", x))
 
-
-Task.of(1) // acts just like a Box
-.map(x => x + 1 ) // in the rejected case it ignores it alltogether
-.chain(x => )
+Task.of(1)
+.map(x => x + 1 )
+.chain(x => Task.of(x + 1))
 .fork(e => console.log("err", e),
       x => console.log("success", x))
+
+
+const launchMissiles = () =>
+  new Task((rej, res) => {
+      console.log("Launch missiles!")
+      res("missile")
+  })
+
+launchMissiles()
+.map( x => x + "!")
+.fork(e => console.log("err", e),
+      x => console.log("success", x))
+
+// if u don't fork, it will jsut not run
+
+
+const app = launchMissiles().map(x => x + "!")
+app.map(x => x + "!").fork(e => console.log("err", e),
+                           x => console.console.log(("success", x));)
