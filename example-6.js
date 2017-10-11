c8775af105a66de9b1d60436e2fc347b3acf4104
Dragos <dragosgn@gmail.com>
2017-10-11 15:44:48 +0200

    ensure failsafe using monoids

--- example-6.js
+++ example-6.js
@@ -5,13 +5,31 @@
 // Can also do this:
 // const res = [1,2].concat([3,4].concat([5.6]))
 
+// const Sum = x =>
+// ({
+//   x,
+//   concat: o =>
+//     Sum(x + o.x),
+//   inspect: () =>
+//     `Sum(${x})`
+// })
+
+// Semi-gropus: type with a concat method that is associative - Do again!
+
 const Sum = x =>
 ({
   x,
-  concat: o =>
-    Sum(x + o.x)
+  concat: ({x: y}) =>
+    Sum(x + o.x),
+  inspect: () =>
+    `Sum(${x})`
 })
 
 const res = Sum(1).concat(Sum(2))
+
+true && false // false
+true && true // true
+
+// Concat is not a safe operation because if there is no first ellement it mcht fail, reduce is however safe
 
 console.log(res)
