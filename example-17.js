36671f81ab6859654eadaae353a96498b65ddf53
Dragos <dragosgn@gmail.com>
2017-10-26 12:14:44 +0200

    fixes

--- example-17.js
+++ example-17.js
@@ -5,5 +5,10 @@ const { List, Map } = require('immutable-ext')
 const httpGet = (path, params) =>
   Task.of(`${path} result`)
 
-Map({home: '/', about:'/about-us', blog: '/blog'})
-.map(route => httpGet(route, {}))
+// Map({home: '/', about:'/about-us', blog: '/blog'})
+// .map(route => httpGet(route, {}))
+
+Map({home: ['/', '/home'], about: ['/about-us'], blog: ['/blog']})
+.traverse(Task.of, routes =>
+  routes.traverse(Task.of, route => httpGet(route, {})))
+.fork(console.error, console.log)
